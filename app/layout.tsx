2cfcec424c32d324541307f944e18e4b47766e61
Ignacio Guri <ignacio.guri@doctari.de>
2023-09-30 01:18:47 +0200

    feat: new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,12 @@ import { cookies } from "next/headers";
 import SupabaseProvider from "@/lib/supabase-provider";
 
 import "@/styles/globals.css";
+import { Metadata } from "next";
 
 // do not cache this layout
 export const revalidate = 0;
 
-export const metadata = {
+export const metadata: Metadata = {
   description: "Oktoberfest attendance registering app",
   title: "ProstCounter 🍻",
 };
@@ -29,13 +30,8 @@ export default async function RootLayout({
         <div className="flex min-h-screen flex-col items-center justify-center py-2">
           <main className="flex w-full flex-1 shrink-0 flex-col items-center justify-center px-8 text-center sm:px-20">
             <h1 className="mb-12 text-5xl font-bold sm:text-6xl">
-              <span className="font-extrabold text-yellow-600">
-                Prost
-              </span>
-              <span className="font-extrabold text-yellow-500">
-                Counter
-              </span>{" "}
-              🍻
+              <span className="font-extrabold text-yellow-600">Prost</span>
+              <span className="font-extrabold text-yellow-500">Counter</span> 🍻
             </h1>
             <SupabaseProvider session={session}>{children}</SupabaseProvider>
           </main>
