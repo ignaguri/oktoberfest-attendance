699a30b1e05d47b32220434866cf79a644e0110f
Ignacio Guri <ignacio.guri@holidu.com>
2025-08-29 19:42:32 +0200

    chore(changelog): restore lost data

--- changelog.ts
+++ changelog.ts
@@ -1,5 +1,31 @@
 export const changelog: Record<string, string[]> = {
   "0.4.1": [
-    "🔧 Version update"
-  ]
+    "🔔 Notifications system",
+    "📲 Installable app improvements",
+    "🎨 New logo",
+  ],
+  "0.3.0": [
+    "🎪 Multi-festival support - Switch between 2024 and 2025 Oktoberfest!",
+    "👤 New user menu with profile and pages access",
+    "🏆 Completely new achievements system!",
+  ],
+  "0.2.5": [
+    "Added link to Oktoberfest map 🗺️",
+    "Smoother page transtitions 🚀",
+  ],
+  "0.2.4": [
+    "You can now delete an attendance in My attendance page 🗑️",
+    "Added Share App button 📣",
+  ],
+  "0.2.3": ["You can set a custom beer cost in your profile! 💶"],
+  "0.2.2": ["New Global leaderboard! 🥇"],
+  "0.2.1": [
+    "Implemented 'What's New' feature 🎉",
+    "Now you can upload beer pictures! 📸",
+    "Introduced group photo galleries 🖼️",
+  ],
+  "0.2.0": [
+    "Enhanced leaderboard with new sorting options",
+    "Fixed various UI bugs",
+  ],
 };
